e5fad9061312d81de8479e4785e834ba0f254e52
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-09-04 11:48:49 -0300

    add rospkg to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='gym_social',
       version='0.0.1',
       install_requires=[
+        'rospkg',
         'gym>=0.2.3',
         'opencv-python==4.1.0.25'
         ]
