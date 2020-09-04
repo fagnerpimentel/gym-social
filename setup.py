c1658e6054942464727bf02b1970bad1b3d2ad42
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-09-04 12:15:01 -0300

    add defusedxml to setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='gym_social',
       version='0.0.1',
       install_requires=[
         'rospkg',
+        'defusedxml'
         'gym>=0.2.3',
         'opencv-python==4.1.0.25'
         ]
