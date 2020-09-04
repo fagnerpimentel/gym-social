63cecb9959ab0fc30ca5148af8106ce36379a928
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-09-04 16:08:20 -0300

    add install_dependencies and remove setup.py

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install -y python3-pip
+
+pip3 install rospkg
+pip3 install defusedxml
+pip3 install gym>=0.2.3
+pip3 install opencv-python==4.1.0.25
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_social',
-      version='0.0.1',
-      install_requires=[
-        'gym>=0.2.3',
-        'opencv-python==4.1.0.25'
-        ]
-)
